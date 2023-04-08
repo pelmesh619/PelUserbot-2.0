900c4bd77847f4230a3766da7bd211b817e77696
Pelmeska Pelmenov <75571195+pelmesh619@users.noreply.github.com>
2023-04-08 23:21:42 +0300

    Moved config file, fixed some bullshit

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 from pyrogram import filters, idle
-
-filters.me = filters.user(1134950789)
-
 from core import Peluserbot
 
 __version__ = '2.0.0-alpha1'
 
 client = Peluserbot(app_version=__version__, config_filename='config.json')
-
-
-@client.on_message(filters.me & filters.command('huh'))
-async def fuck(_, message):
-    await message.reply('h')
-    print('huh', message.from_user)
-    # await message.reply('\n'.join(map(str, app.modules)))
 
 client.run()
